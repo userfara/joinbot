f8a40a939a03f87f85d6f732b8c9c5942808e728
userfara <808elnur@gmail.com>
2024-10-22 23:20:43 +0500

    Updated join.py with API token

--- join.py
+++ join.py
@@ -1,16 +1,15 @@
 import telebot
 from telebot.types import Message
 
+# API tokenini bevosita kiritish (bu tavsiya etilmaydi, lekin agar kerak bo'lsa)
 API_TOKEN = '7920696279:AAFTPkEuNh3oDU1NQGzIQuFF9qiVbjhs4H4'
 
 bot = telebot.TeleBot(API_TOKEN)
 
-# Guruhdagi qo'shilish/tark etish xabarlarini o'chirish uchun handler
 @bot.message_handler(content_types=['new_chat_members', 'left_chat_member'])
 def handle_new_left_members(message: Message):
     try:
         bot.delete_message(message.chat.id, message.message_id)
-        # Avvalgi xabarlar ham o'chiriladi
         if message.reply_to_message:
             bot.delete_message(message.chat.id, message.reply_to_message.message_id)
     except Exception as e:
